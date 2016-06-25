a350dc4e19232d9de157b3f707a9a2251858e63b
Mohammed Sohail <itscrazybob@gmail.com>
2016-06-25 21:18:12 +0400

    Reformated code, commented, improved code quality

--- media.py
+++ media.py
@@ -1,7 +1,15 @@
+# Importing the necessary modules
 import webbrowser
 
+# The class for the movie's data to go in
 class Movie():
+    """
+	Class for Inputs of each movie instance
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, imdb):
+    	"""
+            The constructor for creating variables for each movie instance
+    	"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -9,4 +17,7 @@ class Movie():
         self.imdb_url = imdb
 
     def show_trailer(self):
+    	"""
+	    Method for opening the trailer in user's browser using the 'webbrowser' module.
+    	"""
         webbrowser.open(self.trailer_youtube_url)
